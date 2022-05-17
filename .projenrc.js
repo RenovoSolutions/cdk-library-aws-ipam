674e8d9e3dc247cdb411d086f21e878d0674e01e
Brandon Miller <brandon@digital-reboot.com>
2022-05-17 05:34:23 -0400

    chore(deps): Add `projen` upgrade workflow, update deps, update projen

--- .projenrc.js
+++ .projenrc.js
@@ -18,6 +18,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     workflowOptions: {
       labels: ['auto-approve', 'deps-upgrade'],
     },
+    exclude: ['projen'],
   },
   githubOptions: {
     mergify: true,
@@ -78,4 +79,16 @@ const project = new awscdk.AwsCdkConstructLibrary({
   workflowNodeVersion: '14.17.0',
   gitignore: ['**/__pycache__/**'],
 });
+
+new javascript.UpgradeDependencies(project, {
+  include: ['projen'],
+  taskName: 'upgrade-projen',
+  labels: ['projen-upgrade'],
+  workflow: true,
+  workflowOptions: {
+    schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 2 * * 1']),
+  },
+  pullRequestTitle: 'chore(deps): upgrade projen',
+});
+
 project.synth();
